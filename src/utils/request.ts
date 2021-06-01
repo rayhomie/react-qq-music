1ce6eef5e2b6ca7e2e03ee938e08ffd284a55708
rayhomie <1572801584@qq.com>
2021-06-01 19:39:37 +0800

    生产环境接口配置

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL:
+    process.env.NODE_ENV === 'production' ? 'http://localhost:3333' : 'http://localhost:3333/api',
   headers: {
     // 'Content-Type': 'application/json',
     // Accept: '*/*',
